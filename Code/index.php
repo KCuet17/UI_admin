93906dba77805baf905213a23c651f1ec9263ac6
huyvip2000ht <56397858+huyvip2000ht@users.noreply.github.com>
2021-11-26 21:41:08 +0700

    push tạm

--- Code/index.php
+++ Code/index.php
@@ -9,6 +9,9 @@
         .margin{
             margin-left:10px;
         }
+        .margin-more{
+            margin-left:10px;
+        }
 
 
     </style>
@@ -19,7 +22,7 @@
     
     <div>
         <h1>Add show:</h1>
-        <form action="result.php">
+        <form action="result_huy.php">
         
 
             <label for="filename">File Name:</label><br>
@@ -28,17 +31,54 @@
             <label for="title">Title:</label><br>
             <input type="text" id="title" name="title" ><br><br>
 
-            <label for="author">Author:</label><br>
-            <input type="text" id="author" name="author" ><br><br>
-
             <label for="des">Description:</label><br>
             <input type="text" id="des" name="des" ><br><br>
 
-            <label for="item">Item:</label>
-            <button type="button" class="addmore">Add more item</button>
+
+
+
+
+            <label for="item">Model:</label>
+            <button type="button" class="addmoremodel">Add more model</button>
             
             
-            <div id=items>
+
+            <div id=models>
+                <div class="model" id="model1">
+                    <p class="margin">Model1</p>
+
+
+                    <label class="margin" for="modeltitle">Title:</label>
+                    <input type="text" id='modeltitle'  name="modeltitle[]"><br><br>
+                    
+                    <label class="margin" for="modelauthor">Author:</label>
+                    <input type="text" id='modelauthor' name="modelauthor[]"><br><br>
+
+                    <label class="margin" for="modeldes">Item Description:</label>
+                    <input type="text" id='modeldes' name="modeldes[]"><br><br>
+
+                    <label class="margin" for="modeltime">Time:</label>
+                    <input type="text" id='modeltime' name="modeltime[]"><br><br> 
+                    
+                    <label for="item">Item:</label>
+                    <button type="button" class="addmoreitem">Add more item</button>
+
+                    <div id="items">
+                        <div class="item" id="item1">
+                            <p class="margin">Item1</p>
+                            <label class="margin" for="itemid">ItemID:</label>
+                            <input type="text" id='itemid'  name="itemid[][]"><br><br>                        
+                        </div>                        
+                    </div>
+
+
+                </div>
+            </div>  
+
+
+
+
+            <!-- <div id=items>
                 <div class="item" id="item1">
                     <p class="margin">Item1</p>
                     <label class="margin" for="itemid">ItemID:</label>
@@ -57,7 +97,7 @@
                     <input type="text" id='itemdate' name="itemdate[]"><br><br> 
 
                 </div>
-            </div>  
+            </div>   -->
             <input type="submit" value="Submit"><br>
 
 
@@ -71,7 +111,7 @@
 <script>
     var rowNum = 1;
 
-    $("body").on("click", ".addmore", function() {
+    $("body").on("click", ".addmoremodel", function() {
         rowNum++;
         var $item = $(this).next().children().last();
         var nextHtml = "                <div class='item' id='item"+rowNum+"'>"+
@@ -106,6 +146,50 @@
         $item.after(nextHtml); 
     });
 
+    var itemNum;
+
+    $("body").on("click", ".addmoreitem", function() {
+        rowNum++;
+        var $item = $(this).next().children().last();
+        var nextHtml = "                <div class='item' id='item"+rowNum+"'>"+
+        "<p class='margin'>Item"+rowNum+"</p>"+
+        "<label class='margin' for='itemid'>ItemID:</label>"+
+        "<input type='text'  id='itemid' name='itemid[]'><br><br>"+
+
+        "<label class='margin' for='itemtitle'>Title:</label>"+
+        "<input type='text' id='itemtitle' name='itemtitle[]'><br><br>"+
+
+        "<label class='margin' for='itemdes'>Item Description:</label>"+
+        "<input type='text' id='itemdes' name='itemdes[]'><br><br>"+
+
+        "<label class='margin' for='itemtype'>Item Type:</label>"+
+        "<input type='text' id='itemtype' name='itemtype[]'><br><br>"+
+
+        "<label class='margin' for='itemdate'>Date:</label>"+
+        "<input type='text' id='itemdate' name='itemdate[]'>"+
+        
+        "<button type='button' class='rmbtn'>Remove this item</button><br><br> "+
+        
+    "</div>";
+
+
+
+       // nextHtml.attr('id', 'item' + rowNum);
+        var hasRmBtn = $('.rmbtn', nextHtml).length > 0;
+     //   if (!hasRmBtn) {
+      //  var rm = "<button type='button' class='rmbtn'>Remove</button>"
+      //  $('.addmoreadd', nextHtml).append(rm);
+     //   }
+        $item.after(nextHtml); 
+    });
+
+
+
+
+
+
+
+
     $("body").on("click", ".rmbtn", function() {
         $(this).parents('.item').remove();
     });
@@ -115,145 +199,4 @@
 
 
     
-    </script>
-<script>
-    // function addStep(){
-
-    //     var element = document.getElementById("steps");
-
-    //     var lbType = document.createElement("label");
-    //     lbType.innerHTML = "Type:";
-    //     lbType.classList.add('margin');
-    //    // lbType.for = "type";
-
-    //     var inpType= document.createElement("input");
-    //     inpType.type = "text";
-    //     inpType.id = "type";
-    //     inpType.name = "type";
-        
-    //     var lbHint = document.createElement("label");
-    //     lbHint.innerHTML = "Hint:";
-    //     lbHint.classList.add('margin');
-
-    //     var inpHint= document.createElement("input");
-    //     inpHint.type = "text";
-    //     inpHint.id = "hint";
-    //     inpHint.name = "hint";
-
-    //     var lbCode = document.createElement("label");
-    //     lbCode.innerHTML = "Code:";
-    //     lbCode.classList.add('margin');
-
-    //     var inpCode= document.createElement("input");
-    //     inpCode.type = "text";
-    //     inpCode.id = "code";
-    //     inpCode.name = "code";
-
-    //     var br = document.createElement('br');
-
-    //     element.appendChild(lbType);
-    //     element.appendChild(inpType);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    //     element.appendChild(lbHint);
-    //     element.appendChild(inpHint);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    //     element.appendChild(lbCode);
-    //     element.appendChild(inpCode);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    // }
-
-
-    // function addItem(){
-
-    //     var element = document.getElementById("items");
-
-    //     var lbId = document.createElement("label");
-    //     lbId.innerHTML = "ItemID:";
-    //     lbId.classList.add('margin');
-    //     // lbType.for = "type";
-
-    //     var inpId= document.createElement("input");
-    //     inpId.type = "text";
-    //     inpId.id = "itemid";
-    //     inpId.name = "itemid";
-
-    //     var lbTitle = document.createElement("label");
-    //     lbTitle.innerHTML = "Title:";
-    //     lbTitle.classList.add('margin');
-
-    //     var inpTitle= document.createElement("input");
-    //     inpTitle.type = "text";
-    //     inpTitle.id = "title";
-    //     inpTitle.name = "title";
-
-    //     var lbDes = document.createElement("label");
-    //     lbDes.innerHTML = "Item Des:";
-    //     lbDes.classList.add('margin');
-
-    //     var inpDes= document.createElement("input");
-    //     inpDes.type = "text";
-    //     inpDes.id = "itemdes";
-    //     inpDes.name = "itemdes";
-
-    //     var lbType = document.createElement("label");
-    //     lbType.innerHTML = "Item Type:";
-    //     lbType.classList.add('margin');
-
-    //     var inpType= document.createElement("input");
-    //     inpType.type = "text";
-    //     inpType.id = "itemtype";
-    //     inpType.name = "itemtype";
-
-    //     var lbDate = document.createElement("label");
-    //     lbDate.innerHTML = "Item Date:";
-    //     lbDate.classList.add('margin');
-
-    //     var inpDate= document.createElement("input");
-    //     inpDate.type = "text";
-    //     inpDate.id = "itemdate";
-    //     inpDate.name = "itemdate";
-
-    //     var br = document.createElement('br');
-
-    //     element.appendChild(lbId);
-    //     element.appendChild(inpId);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    //     element.appendChild(lbTitle);
-    //     element.appendChild(inpTitle);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    //     element.appendChild(lbDes);
-    //     element.appendChild(inpDes);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    //     element.appendChild(lbType);
-    //     element.appendChild(inpType);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    //     element.appendChild(lbDate);
-    //     element.appendChild(inpDate);
-    //     element.appendChild(document.createElement('br'));
-    //     element.appendChild(document.createElement('br'));
-
-    // }
-
-    // var i=0;
-    // $('.menuHeader').each(function(){
-    //     i++;
-    //     var newID='menu'+i;
-    //     $(this).attr('id',newID);
-    //     $(this).val(i);
-    // });
-
 </script>
