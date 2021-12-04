1bcc23cd9ad3133aea3d8e7f827d4da679d35450
KCuet17 <48212263+KCuet17@users.noreply.github.com>
2021-12-04 21:24:44 +0700

    admin_ui0.5
    thay doi jq thanh domjs

--- Code/index.php
+++ Code/index.php
@@ -56,23 +56,20 @@
         foreach ($obj['trealet']['models'] as $value) {
         ?>
             <input type="text" class="margin" value="<?= $value['modeltime'] ?> : <?= $value['modeltitle'] ?>">
-            <form action="edit_model.php"><input type="hidden" name="pos" value="<?= $count ?>"><input type="submit" value="Chỉnh sửa"></form>
+            <button type="button" id="<?= $count ?>" onclick="editmodel1(this.id)">Chinh sua</button>
             <form action="delete.php"><input type="hidden" name="pos" value="<?= $count ?>"><input type="submit" value="Xóa"></form>
         <?php
             $count++;
         }
         ?>
     </div>
-    <button type="button" class="addmoremodel">Thêm một mục mới</button> <br>
-    <div class="add"><div></div></div>
-</body>
-</html>
+    <hr>
+    <button type="button" onclick="addmoremodel()">Thêm một mục mới</button> <br>
+    <div id="add"></div>
+</body></html>
 <script>
     ///////////model
-
-    $("body").on("click", ".addmoremodel", function() {
-        alert(ok);
-        var $item = $(this).next().next().children().last();
+        function addmoremodel(){       
         var nextHtml = "<form action='add.php'>" +
             "<div id=models>" +
             '<div class="model" id="model">' +
@@ -100,47 +97,49 @@
             "<input type='text' id='id' name='id' placeholder='Cách nhau bởi dấu cách'><br><br>" +
             '</div></div>' +
             '<input type="submit" value="Thêm">   ' +
-            "<button type='button' class='rmbtn'>Hủy</button><br><br> " +
+            "<button type='button' onclick='remove()'>Hủy</button><br><br> " +
             '</form>';
-        $item.after(nextHtml);
-    });
+            document.getElementById("add").innerHTML = nextHtml;
+           }
     
-    $("body").on("click", ".rmbtn", function() {
-        $(this).parents(".add").remove();
-    });
-    function editmodel() {
-        alert(0);
-        var nextHtml2 =
-            "<form action='editmodel.php'>" +
+    function remove(){
+        document.getElementById("add").innerHTML = "";
+    }
+    function editmodel1(x) {
+        var nextHtml2 = "<form action='edit_model.php'>" +
             "<div >" +
             '<div >' +
             '<h4>Thông tin mục hiển thị</h4>' +
 
             "<label class='margin' for='modeltitle'>Tiêu đề mục hiển thị:</label>" +
-            "<input type='text' id='modeltitle'  name='modeltitle'><br><br>" +
+            "<input type='text' id='modeltitle' name='modeltitle' placeholder='' ><br><br>" +
 
             "<label class='margin' for='modeldes'>Mô tả mục hiển thị:</label>" +
-            "<input type='textarea' id='modeldes' name='modeldes'><br><br>" +
+            "<input type='textarea' id='modeldes' name='modeldes' placeholder=''><br><br>" +
 
             "<label class='margin' for='modeltime'>Thời gian:</label>" +
-            "<input type='text' id='modeltime' name='modeltime'><br><br>" +
+            "<input type='text' id='modeltime' name='modeltime' placeholder=''><br><br>" +
 
             "<label class='margin' for='modelImg'>ID ảnh nền:</label>" +
-            "<input type='text' id='backgroundImg' name='backgroundImg'><br><br>" +
+            "<input type='text' id='backgroundImg' name='backgroundImg'placeholder=''><br><br>" +
 
             "<label class='margin' for='itemtype'>Định dạng các item: </label>" +
             '<select name="itemtype" for="itemtype">' +
+            '<option value="" selected></option>' +
             '<option value="video">Video</option>' +
-            '<option value="img" selected>Ảnh</option>' +
+            '<option value="img">Ảnh</option>' +
             '</select> <br><br>' +
 
-            "<label class='margin' for='modelImg'>Danh sách ID các item:</label>" +
-            "<input type='text' id='id' name='id' placeholder='Cách nhau bởi dấu cách'><br><br>" +
+            "<label class='margin' for='id'>Danh sách ID các item:</label>" +
+            "<input type='text' id='id' name='id' placeholder=''><br><br>" +
 
             '</div></div>' +
             '<input type="submit" value="Thêm">   ' +
-            "<button type='button' class='rmbtn'>Hủy</button><br><br> " +
+            "<button type='button' onclick='remove1()'>Hủy</button><br><br> " +
             '</form>';
-        //document.getElementsByClassName("add").children().last().after();
+            //document.getElementById("add").innerHTML = nextHtml2;
+    }
+    function remove1(){
+        document.getElementById("add").innerHTML = "";
     }
 </script>
